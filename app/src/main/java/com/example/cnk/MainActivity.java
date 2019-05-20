5aa75a1181986fc7e9ac66febc8ad164bda05d6b
николай елопов <e.nikolay.minsk@gmail.com>
2019-05-20 17:37:03 +0300

    v2

--- app/src/main/java/com/example/cnk/MainActivity.java
+++ app/src/main/java/com/example/cnk/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.cnk;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,39 +17,48 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static int MAX_MESSAGE_LENGTH =150;
+    private static int MAX_MESSAGE_LENGTH = 150;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("messages");
-    Button btnInput;
+    Button btnInput,btnClear;
     EditText editMsg;
     ArrayList<String> messages = new ArrayList<>();
     RecyclerView recMsgs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnClear = (Button) findViewById(R.id.btnClear);
         btnInput = (Button) findViewById(R.id.btnSndMsg);
         editMsg = (EditText) findViewById(R.id.editMsg);
         recMsgs = (RecyclerView) findViewById(R.id.recyclerMsg);
         recMsgs.setLayoutManager(new LinearLayoutManager(this));
         final DataAdapter dataAdapter = new DataAdapter(this, messages);
         recMsgs.setAdapter(dataAdapter);
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.removeValue();
+                dataAdapter.notifyDataSetChanged();
+                messages.clear();
+            }
+        });
         btnInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String msg = String.valueOf(editMsg.getText());
-                if(msg.equals("")){
-                    Toast.makeText(getApplicationContext(),"Пустое сообщение", Toast.LENGTH_SHORT).show();
+                if (msg.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Пустое сообщение", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (msg.length()>MAX_MESSAGE_LENGTH){
-                    Toast.makeText(getApplicationContext(),"Слишком много символов", Toast.LENGTH_SHORT).show();
+                if (msg.length() > MAX_MESSAGE_LENGTH) {
+                    Toast.makeText(getApplicationContext(), "Слишком много символов", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 myRef.push().setValue(msg);
@@ -72,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
+                messages.clear();
             }
 
             @Override
